f423fabb0fcb6660cfeaec6813ec8312d02c14db
Ezeki Zibzibadze <ez@jok.io>
2025-06-01 16:15:07 +0400

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,38 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      crypto: 'false',
-    },
-  },
   publicDir: 'test',
   server: {
     open: '/index.html', // or 'test/index.html' if kept in /test
   },
+
   build: {
+    modulePreload: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: '@jokio/sdk',
       fileName: format => `jokio.sdk.${format}.js`,
     },
-    // rollupOptions: {
-    //   output: {
-    //     globals: {
-    //       // Example: react: 'React'
-    //     },
-    //   },
-    // },
   },
+
+  plugins: [
+    {
+      name: 'replace-crypto',
+      generateBundle(options, bundle) {
+        Object.keys(bundle).forEach(fileName => {
+          const chunk = bundle[fileName]
+          if (chunk.type === 'chunk') {
+            chunk.code = chunk.code.replace(
+              /[a-zA-Z_$][a-zA-Z0-9_$]*\s*=\s*require\(["']crypto["']\)/g,
+              'crypto = window.crypto || globalThis.crypto',
+            )
+            chunk.code = chunk.code.replace(
+              /require\(["']crypto["']\)/g,
+              '(window.crypto || globalThis.crypto)',
+            )
+          }
+        })
+      },
+    },
+  ],
 })
